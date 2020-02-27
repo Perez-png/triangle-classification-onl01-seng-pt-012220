f24f29858355e775283092d8ebfa4ffe25a2e4e2
Xavier Perez <xperez007@gmail.com>
2020-02-27 18:05:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,9 @@
 class Triangle
-  attr_accessor :Triangle1=90, :Triangle2=45, :Triangle3=45
-    Triangle1.new(90)
-    Triangle2.new(45)
-    Triangle3.new(45)
-    
-  def initialize(:Triangle1, :Triangle2, :Triangle3)
-    
-  end
+  attr_accessor expect(Triangle.new(2, 2, 2).kind).to eq(:equilateral)
   
 end
+
+  def initialize(:Triangle1, :Triangle2, :Triangle3)
 
 end
 
