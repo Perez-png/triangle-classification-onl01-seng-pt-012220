f7b7c49744822d59a669347ec7c70345826071f7
Xavier Perez <xperez007@gmail.com>
2020-02-27 18:02:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,13 @@
 class Triangle
   attr_accessor :Triangle1=90, :Triangle2=45, :Triangle3=45
-    puts triangle1 = 90 
-    puts triangle2 = 45
-    puts triangle3 = 45
-
-
-
+    Triangle1.new(90)
+    Triangle2.new(45)
+    Triangle3.new(45)
+    
+  def initialize(:Triangle1, :Triangle2, :Triangle3)
+    
+  end
+  
 end
 
 end
